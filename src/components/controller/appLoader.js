07c537fe6f72f99e2c1b9cc620b3c8b4e8608c5e
Dmitry Rukovichnikov <drukavichnikau@exadel.com>
2022-12-07 14:50:12 +0400

    initial commit

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '5bb470c44d244ea68261255a8222a824', // получите свой ключ https://newsapi.org/
         });
     }
 }
